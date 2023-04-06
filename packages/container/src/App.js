688f01025c2a376d645a88fdbce74de6143ae3f5
Alexey Magdich <alx.magdich@gmail.com>
2023-04-06 14:21:32 +0200

    Update imports

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Marketing from "./components/Marketing";
 import Header from "./components/Header";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
+  seed: "co",
 });
 
 export default function App() {
