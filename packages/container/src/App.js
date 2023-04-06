aca8689a8a97fab5adb8f49911c99553b88842f8
Alexey Magdich <alx.magdich@gmail.com>
2023-04-06 13:56:44 +0200

    Make production class names unique

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Marketing from "./components/Marketing";
 import Header from "./components/Header";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <Marketing />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Marketing />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
